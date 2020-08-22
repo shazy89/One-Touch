ae182eee9a901c75d468418c2884b963189777ce
shazy89 <erdo.shazy123@gmail.com>
2020-08-22 14:58:54 -0400

    fix routs, fix typo

--- app/controllers/select_items_controller.rb
+++ app/controllers/select_items_controller.rb
@@ -5,50 +5,52 @@ class SelectItemsController < ApplicationController
 
   end
 
+  def update
+   if  @item.update(select_item_params)
+     redirect_to table_path(@item.table)
+   else
+     redirect_to root_path
+   end
+ end
    
-   def update
-      if  @item.update(select_item_params)
-          #binding.pry
-        redirect_to table_path(@item.table)
-      else
-        redirect_to root_path
-      end
-   end
+ def destroy  
+  @item.destroy
+  redirect_to table_path(@item.table)
+ end
 
-   def destroy  
-    @item.destroy
-    redirect_to table_path(@item.table)
-   end
-
-
-   def select_item
-    @user= current_user
-    @i = SelectItem.product_and_table(params)
-     if @i.present?
-         @i[0].quantity += params[:quantity].to_i
-         @i[0].save 
-         redirect_to table_path(@i[0].table_id)
-      else
-        if @item = SelectItem.create(quantity: params[:quantity], product_id: params[:product_id], table_id: params[:table_id])
-          redirect_to table_path(@item.table_id)
-         else 
-           redirect_to root_path
-         end
+ def select_item
+  @user= current_user
+  @i = SelectItem.product_and_table(params)
+   if @i.present?
+       @i[0].quantity += params[:quantity].to_i
+       @i[0].save 
+       redirect_to table_path(@i[0].table_id)
+    else
+      if @item = SelectItem.create(quantity: params[:quantity], product_id: params[:product_id], table_id: params[:table_id])
+        redirect_to table_path(@item.table_id)
+       else 
+         redirect_to root_path
        end
-    end
-         
-    private
-    
-    def set_selected_item
-       @item = SelectItem.find_by_id(params[:id])
-    end
-    
-    def  select_item_params
-      params.require(:select_item).permit(:table_id, :product_id, :quantity)
-    end
-        
      end
-           
+  end
+       
+  private
+  
+  def set_selected_item
+     @item = SelectItem.find_by_id(params[:id])
+  end
+  
+  def  select_item_params
+    params.require(:select_item).permit(:table_id, :product_id, :quantity)
+  end
+
+  def   
+      
+   end
+         
+
+
+
          
         
    
